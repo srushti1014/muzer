ff869393f09c20403fc03de870211bd352967e9f
srushti <srushtipatel4040@gmail.com>
2025-07-31 16:51:14 +0530

    creator fixi

--- app/creator/[creatorId]/page.tsx
+++ app/creator/[creatorId]/page.tsx
@@ -1,16 +1,30 @@
-"use client"
-
 import StreamView from '@/components/StreamView'
 import { FC } from 'react'
 
-interface PageProps {
+type Props = {
   params: {
     creatorId: string
   }
 }
 
-const CreatorPage: FC<PageProps> = ({ params }) => {
+const Creator: FC<Props> = ({ params }) => {
   return <StreamView creatorId={params.creatorId} playVideo={false} />
 }
 
-export default CreatorPage
+export default Creator
+// import StreamView from '@/components/StreamView'
+// import React from 'react'
+
+// const Creator = ({ params: {
+//     creatorId
+// } }: {
+//     params: {
+//         creatorId: string
+//     }
+// }) => {
+//     return (
+//         <StreamView creatorId={creatorId} playVideo={false} />
+//     )
+// }
+
+// export default Creator
