bc48d95b3e2019d16aa54c58b0a340cf036c1194
srushti <srushtipatel4040@gmail.com>
2025-08-01 16:37:06 +0530

    multie spaces

--- app/dashboard/[spaceId]/page.tsx
+++ app/dashboard/[spaceId]/page.tsx
@@ -3,8 +3,13 @@ import StreamView from "@/components/StreamView"
 import axios from "axios";
 import { useEffect, useState } from "react"
 
+interface PageProps {
+  params: {
+    spaceId: string;
+  };
+}
 
-export default function Dashboard({ params: { spaceId } }: { params: { spaceId: string } }) {
+export default function Page({ params }: PageProps) {
 
   const [creatorId, setCreatorId] = useState<string | null>(null);
   const [loading1, setLoading1] = useState(true);
@@ -12,7 +17,7 @@ export default function Dashboard({ params: { spaceId } }: { params: { spaceId: 
   useEffect(() => {
     async function fetchHostId() {
       try {
-        const res = await axios.get(`/api/space/?spaceId=${spaceId}`);
+        const res = await axios.get(`/api/space/?spaceId=${params.spaceId}`);
         console.log("res of space for hostId:", res)
         // if (!res) {
         //     throw new Error(res.message || "Failed to retreive space's host id");
@@ -31,6 +36,42 @@ export default function Dashboard({ params: { spaceId } }: { params: { spaceId: 
 
   // const creatorId ="8d96adf1-28ea-4a88-8ef0-c47ab0518556"
   return (
-    <StreamView creatorId={creatorId as string} playVideo={true} spaceId={spaceId} />
+    <StreamView creatorId={creatorId as string} playVideo={true} spaceId={params.spaceId} />
   )
 }
+// "use client"
+// import StreamView from "@/components/StreamView"
+// import axios from "axios";
+// import { useEffect, useState } from "react"
+
+
+// export default function Dashboard({ params: { spaceId } }: { params: { spaceId: string } }) {
+
+//   const [creatorId, setCreatorId] = useState<string | null>(null);
+//   const [loading1, setLoading1] = useState(true);
+
+//   useEffect(() => {
+//     async function fetchHostId() {
+//       try {
+//         const res = await axios.get(`/api/space/?spaceId=${spaceId}`);
+//         console.log("res of space for hostId:", res)
+//         // if (!res) {
+//         //     throw new Error(res.message || "Failed to retreive space's host id");
+//         //   }
+//         setCreatorId(res.data.hostId)
+
+//       } catch (error) {
+
+//       }
+//       finally {
+//         setLoading1(false)
+//       }
+//     }
+//     fetchHostId();
+//   }, [])
+
+//   // const creatorId ="8d96adf1-28ea-4a88-8ef0-c47ab0518556"
+//   return (
+//     <StreamView creatorId={creatorId as string} playVideo={true} spaceId={spaceId} />
+//   )
+// }
