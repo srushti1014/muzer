5ff54669dcc3129f958e34b952df27e730ac7833
srushti <srushtipatel4040@gmail.com>
2025-08-01 16:26:15 +0530

    multie spaces

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,24 +1,12 @@
-"use client";
-
-import { useSearchParams } from "next/navigation";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import AuthScreen from "@/components/auth/auth-screen";
 import { SignInFlow } from "@/types/auth-type";
+import AuthScreen from "@/components/auth/auth-screen";
 
-export default function AuthPage() {
-  const session = useSession();
-  const router = useRouter();
-  const searchParams = useSearchParams();
-
-  const authType = searchParams.get("authType") as SignInFlow | null;
-
-  if (session.status === "authenticated") {
-    router.push("/");
-    return null;
-  }
-
-  return <AuthScreen authType={authType ?? "signIn"} />;
+export default function AuthPage({
+  searchParams,
+}: {
+  searchParams: { authType: SignInFlow; mailId?: string };
+}) {
+  return <AuthScreen authType={searchParams.authType} />;
 }
 
 // "use client";
